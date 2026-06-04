6eea36487e1fcb884927ef9eba52ff8edb1cb113
2020mtran <2020mtran@gmail.com>
2026-06-04 14:40:21 -0700

    Project cards now have better borders.

--- portfolio/src/components/ui/projectShowcase.tsx
+++ portfolio/src/components/ui/projectShowcase.tsx
@@ -14,82 +14,84 @@ export default function ProjectCard({
 }: ProjectCardProps) {
 
     return (
-        <div className="relative flex overflow-hidden rounded-3xl bg-[linear-gradient(190deg,#252525,#121212)] border border-[#3d3d3d] shadow-[0_0_40px_rgba(255,255,255,0.03)] min-h-87.5 w-full transition-all
-        duration-300 hover:-translate-y-1 hover:border-[#5a5a5a] hover:shadow-[0_0_60px_rgba(255,255,255,0.06)]">
+        <div className="rounded-[32px] p-2 border border-white/10 shadow-[0_40px_80px_rgba(0,0,0,0.5)] transition-all
+            duration-300 hover:-translate-y-1 hover:border-[#5a5a5a] hover:shadow-[0_0_60px_rgba(255,255,255,0.06)]">
+            
+            <div className="relative flex overflow-hidden rounded-3xl bg-[linear-gradient(190deg,#252525,#121212)] border border-[#3d3d3d] shadow-[0_0_40px_rgba(255,255,255,0.03)] min-h-87.5 w-full">
 
-        {/* Image Side */}
-        <div className="relative basis-[65%] overflow-hidden" style={{
-            clipPath: "polygon(0 0, 92% 0, 88% 100%, 0 100%)",
-        }}>
+                {/* Image Side */}
+                <div className="relative basis-[65%] overflow-hidden" style={{
+                    clipPath: "polygon(0 0, 92% 0, 88% 100%, 0 100%)",
+                }}>
 
-            <Image
-                src={image}
-                alt="Project Screenshot"
-                width={1200}
-                height={800}
-                className="w-full h-full object-cover"
-                />
-
-            {/* Diagonal Divider */}
-            {/* <div
-                className="absolute top-0 -right-px h-full w-20"
-                style={{
-                    clipPath: "polygon(40% 0%, 100% 0%, 100% 100%, 0% 100%)",
-                }}
-            /> */}
-        </div>
-
-        {/* Content Side */}
-        <div className="relative basis-[40%] py-8 pr-8 flex flex-col">
-
-            <h2 className="text-4xl font-bold text-gray-300">
-                {title}
-            </h2>
-
-            {/* Description Header */}
-            <div className="flex items-center gap-3 mt-6">
-                <div className="w-8 h-0.5 bg-[#f2f2f2cc]" />
-
-                <span className="uppercase tracking-[0.2em] text-[#f2f2f2cc] font-semibold">
-                    Description
-                </span>
-            </div>
-
-            <p className="mt-2 text-lg text-[#f2f2f280]">
-                {description}
-            </p>
-
-            {/* Tech Header */}
-            <div className="flex items-center gap-3 mt-6">
-                <div className="w-8 h-0.5 bg-[#f2f2f2cc]" />
-
-                <span className="uppercase tracking-[0.2em] text-[#f2f2f2cc] font-semibold">
-                    Built With
-                </span>
-            </div>
-
-            {/* Tech Icons */}
-            <div className="flex gap-4 mt-2">
-                {iconsArray.map((icon, index) => (
                     <Image
-                        key={index}
-                        src={icon}
-                        alt="Technology Icon"
-                        width={40}
-                        height={40}
-                    />
-                ))}
+                        src={image}
+                        alt="Project Screenshot"
+                        width={1200}
+                        height={800}
+                        className="w-full h-full object-cover"
+                        />
 
-                <a
-                href={githubLink}
-                target="_blank"
-                className="ml-auto"
-                >
-                    <FaGithub className="text-4xl text-white hover:scale-110 transition-transform" />
-                </a>
+                    {/* Diagonal Divider */}
+                    {/* <div
+                        className="absolute top-0 -right-px h-full w-20"
+                        style={{
+                            clipPath: "polygon(40% 0%, 100% 0%, 100% 100%, 0% 100%)",
+                        }}
+                    /> */}
+                </div>
+
+                {/* Content Side */}
+                <div className="relative basis-[40%] py-8 pr-8 flex flex-col">
+
+                    <h2 className="text-4xl font-bold text-gray-300">
+                        {title}
+                    </h2>
+
+                    {/* Description Header */}
+                    <div className="flex items-center gap-3 mt-6">
+                        <div className="w-8 h-0.5 bg-[#f2f2f2cc]" />
+
+                        <span className="uppercase tracking-[0.2em] text-[#f2f2f2cc] font-semibold">
+                            Description
+                        </span>
+                    </div>
+
+                    <p className="mt-2 text-lg text-[#f2f2f280]">
+                        {description}
+                    </p>
+
+                    {/* Tech Header */}
+                    <div className="flex items-center gap-3 mt-6">
+                        <div className="w-8 h-0.5 bg-[#f2f2f2cc]" />
+
+                        <span className="uppercase tracking-[0.2em] text-[#f2f2f2cc] font-semibold">
+                            Built With
+                        </span>
+                    </div>
+
+                    {/* Tech Icons */}
+                    <div className="flex gap-4 mt-2">
+                        {iconsArray.map((icon, index) => (
+                            <Image
+                                key={index}
+                                src={icon}
+                                alt="Technology Icon"
+                                width={40}
+                                height={40}
+                            />
+                        ))}
+
+                        <a
+                        href={githubLink}
+                        target="_blank"
+                        className="ml-auto"
+                        >
+                            <FaGithub className="text-4xl text-white hover:scale-110 transition-transform" />
+                        </a>
+                    </div>
+                </div>
             </div>
-
-        </div>
         </div>
     );
 }
