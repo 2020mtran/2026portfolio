c4617b0a9eb9ba0da648fc6322066b4740380240
2020mtran <2020mtran@gmail.com>
2026-06-15 14:14:36 -0700

    Highlights portion complete

--- portfolio/src/app/projects/wuwaNetwork/page.tsx
+++ portfolio/src/app/projects/wuwaNetwork/page.tsx
@@ -33,12 +33,114 @@ export default function WuwaNetwork() {
                 <div className="z-2 opacity-20 mix-blend-overlay bg-[url('/noise.png')] [background-size:240px] absolute inset-0 pointer-events-none" />
                 <div className="z-4 bg-[linear-gradient(to_bottom,transparent,var(--background))] h-60 absolute inset-x-0 bottom-0 top-auto" />
             </div>
-            <div className="flex flex-row p-64 gap-16">
+            <div className="flex flex-row px-64 pt-32 gap-16">
                 <div className="flex flex-col gap-6">
                     <h2 className="text-white text-lg">Vision</h2>
-                    <h2 className="text-white opacity-60 text-md">One day, I decided I wanted to showcase my character builds from a game called Wuthering Waves to my friends. I looked for websites to do so and the few I found were inefficient.</h2>
+                    <h2 className="text-white opacity-60 text-md">One day, I wanted to showcase my character builds from a game called Wuthering Waves to my friends. I looked for websites to do so and the few I found were inefficient.</h2>
                     <h2 className="text-white opacity-60 text-md">The websites either had fully manual input or had an inaccurate scanner.</h2>
                     <h2 className="text-white opacity-60 text-md">As a challenge, I decided to make a website to solve these problems. It would be fully automatic with a singular upload and the scanner would be fine-tuned to ensure accuracy.</h2>
+                </div>
+                <div className="flex flex-col gap-6">
+                    <h2 className="text-white text-lg">Overview</h2>
+                    <h2 className="text-white opacity-60 text-md">- Allows users to upload an image of their in-game stats and extract data using Optical Character Recognition (OCR) with ~97% accuracy (~100% with post-processing). Testing done through voluntary users in an online community. </h2>
+                    <h2 className="text-white opacity-60 text-md">- 120+ unique data points are compiled and summarized in a card with total values and a stylish display.</h2>
+                    <h2 className="text-white opacity-60 text-md">- Intricate testing done through voluntary users in an online community.</h2>
+                </div>
+            </div>
+            <div className="flex flex-col px-64 gap-16">
+                <div className="flex flex-col items-center gap-6">
+                    <h2 className="text-white text-2xl">Highlights</h2>
+                    <div className="rounded-[32px] p-4 border border-white/10 shadow-[0_40px_80px_rgba(0,0,0,0.5)] transition-all
+                    duration-300 hover:-translate-y-1 hover:border-[#5a5a5a] hover:shadow-[0_0_60px_rgba(255,255,255,0.06)]">
+                    
+                        <div className="relative flex flex-col overflow-hidden rounded-3xl bg-[linear-gradient(190deg,#252525,#121212)] border border-[#3d3d3d] shadow-[0_0_40px_rgba(255,255,255,0.03)] min-h-87.5 w-full">
+                            <Image
+                                src="/WuwaNetworkExample1.png"
+                                alt="Project Screenshot"
+                                width={1200}
+                                height={800}
+                                className="w-full h-full object-cover"
+                            />
+                        </div>
+                        <h2 className="text-white opacity-50 text-xs pt-4 pb-10 text-center">Final Output Example</h2>
+
+                        <div className="relative flex flex-col overflow-hidden rounded-3xl bg-[linear-gradient(190deg,#252525,#121212)] border border-[#3d3d3d] shadow-[0_0_40px_rgba(255,255,255,0.03)] min-h-87.5 w-full">
+                            <Image
+                                src="/WuwaNetworkExample2.png"
+                                alt="Project Screenshot"
+                                width={1200}
+                                height={800}
+                                className="w-full h-full object-cover"
+                            />
+                        </div>
+                        <h2 className="text-white opacity-50 text-xs pt-4 pb-10 text-center">Showcase of Different Outputs</h2>
+
+                        <div className="relative flex flex-col overflow-hidden rounded-3xl bg-[linear-gradient(190deg,#252525,#121212)] border border-[#3d3d3d] shadow-[0_0_40px_rgba(255,255,255,0.03)] min-h-87.5 w-full">
+                            <Image
+                                src="/WuwaNetworkExample3.png"
+                                alt="Project Screenshot"
+                                width={1200}
+                                height={800}
+                                className="w-full h-full object-cover"
+                            />
+                        </div>
+                        <h2 className="text-white opacity-50 text-xs pt-4 pb-10 text-center">Generator Page</h2>
+
+                        <div className="relative flex flex-col overflow-hidden rounded-3xl bg-[linear-gradient(190deg,#252525,#121212)] border border-[#3d3d3d] shadow-[0_0_40px_rgba(255,255,255,0.03)] min-h-87.5 w-full">
+                            <Image
+                                src="/WuwaNetworkExample4.png"
+                                alt="Project Screenshot"
+                                width={1200}
+                                height={800}
+                                className="w-full h-full object-cover"
+                            />
+                        </div>
+                        <h2 className="text-white opacity-50 text-xs pt-4 pb-10 text-center">Users can confirm their stats before generating the card.</h2>
+
+                        <div className="relative flex flex-col overflow-hidden rounded-3xl bg-[linear-gradient(190deg,#252525,#121212)] border border-[#3d3d3d] shadow-[0_0_40px_rgba(255,255,255,0.03)] min-h-87.5 w-full">
+                            <Image
+                                src="/WuwaNetworkExample5.png"
+                                alt="Project Screenshot"
+                                width={1200}
+                                height={800}
+                                className="w-full h-full object-cover"
+                            />
+                        </div>
+                        <h2 className="text-white opacity-50 text-xs pt-4 pb-10 text-center">FAQ Page</h2>
+
+                        <div className="relative flex flex-col overflow-hidden rounded-3xl bg-[linear-gradient(190deg,#252525,#121212)] border border-[#3d3d3d] shadow-[0_0_40px_rgba(255,255,255,0.03)] min-h-87.5 w-full">
+                            <Image
+                                src="/WuwaNetworkExample6.png"
+                                alt="Project Screenshot"
+                                width={1200}
+                                height={800}
+                                className="w-full h-full object-cover"
+                            />
+                        </div>
+                        <h2 className="text-white opacity-50 text-xs pt-4 pb-10 text-center">A few questions users may have.</h2>
+
+                        <div className="relative flex flex-col overflow-hidden rounded-3xl bg-[linear-gradient(190deg,#252525,#121212)] border border-[#3d3d3d] shadow-[0_0_40px_rgba(255,255,255,0.03)] min-h-87.5 w-full">
+                            <Image
+                                src="/WuwaNetworkExample7.png"
+                                alt="Project Screenshot"
+                                width={1200}
+                                height={800}
+                                className="w-full h-full object-cover"
+                            />
+                        </div>
+                        <h2 className="text-white opacity-50 text-xs pt-4 pb-10 text-center">Project Resources Page</h2>
+
+                        <div className="relative flex flex-col overflow-hidden rounded-3xl bg-[linear-gradient(190deg,#252525,#121212)] border border-[#3d3d3d] shadow-[0_0_40px_rgba(255,255,255,0.03)] min-h-87.5 w-full">
+                            <Image
+                                src="/WuwaNetworkExample8.png"
+                                alt="Project Screenshot"
+                                width={1200}
+                                height={800}
+                                className="w-full h-full object-cover"
+                            />
+                        </div>
+                        <h2 className="text-white opacity-50 text-xs pt-4 pb-10 text-center">A few resources to provide the user with a look behind-the-scenes of my project including timeline of events and ideas.</h2>
+                    </div>
                 </div>
                 <div className="flex flex-col gap-6">
                     <h2 className="text-white text-lg">Overview</h2>
