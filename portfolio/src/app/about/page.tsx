66f6d3deaee6d1cec41e59b8479a9a1f472ebbf7
2020mtran <2020mtran@gmail.com>
2026-07-01 12:05:28 -0700

    Updated about me page

--- portfolio/src/app/about/page.tsx
+++ portfolio/src/app/about/page.tsx
@@ -19,7 +19,7 @@ export default function about() {
                 <div className="flex flex-row lg:px-32 xl:px-64 gap-3 lg:gap-6 mt-16">
                     <div className="flex flex-col gap-3 lg:gap-6 w-[45%]">
                         <h1 className="text-lg self-baseline text-[#f2f2f242]">
-                            <span className="text-3xl text-white drop-shadow-[0_0_8px_rgba(255,255,255,0.9)]">
+                            <span className="text-2xl text-white drop-shadow-[0_0_8px_rgba(255,255,255,0.9)]">
                                 •
                             </span>{" "} 
                         About Me</h1>
@@ -52,7 +52,7 @@ export default function about() {
                         </div>
                         <h1 className="text-md md:text-xl 3xl:text-3xl opacity-90 px-12 mt-8">What am I doing now?</h1>
                         <h1 className="text-md md:text-xl 3xl:text-3xl opacity-90 px-12 text-[#f2f2f29b]">After taking a year and a half off of school to focus on self-improvement, I decided it was time for me to make my next major step in life.</h1>
-                        <h1 className="text-md md:text-xl 3xl:text-3xl opacity-90 px-12 text-[#f2f2f29b]">Currently, I am purusing a Master's in Computer Science back at CSUF. I can now take the opportunities that I wished I could have taken years ago as my health has significantly improved since then.</h1>
+                        <h1 className="text-md md:text-xl 3xl:text-3xl opacity-90 px-12 text-[#f2f2f29b]">Currently, I am purusing a Master's in Computer Science back at CSUF. I can now take the opportunities that I wished I could had taken years ago because my health has significantly improved since then.</h1>
                         <h1 className="text-md md:text-xl 3xl:text-3xl opacity-90 px-12 text-[#f2f2f29b] mb-8">In the Fall semester of 2026, I will be a Teaching Associate (basically a pseudo-professor), teaching Artificial Intelligence to 4th year undergraduate students and my fellow graduate student peers.</h1>
                         <div className="rounded-[32px] md:p-4 border border-white/10 shadow-[0_40px_80px_rgba(0,0,0,0.5)] transition-all
                         duration-300 hover:-translate-y-1 hover:border-[#5a5a5a] hover:shadow-[0_0_60px_rgba(255,255,255,0.06)]">
@@ -66,6 +66,8 @@ export default function about() {
                                 />
                             </div>
                         </div>
+                        <h1 className="text-md md:text-xl 3xl:text-3xl opacity-90 px-12 mt-8">Thank you for reading!</h1>
+                        <h1 className="text-md md:text-xl 3xl:text-3xl opacity-90 px-12 text-[#f2f2f29b] mb-8">All these photos were taken by me during my time in university.</h1>
                     </div>
                     <div className="flex flex-col gap-3 lg:gap-6 w-[55%] py-32">
                         <h1 className="text-md md:text-xl 3xl:text-3xl text-center opacity-90 px-16 mb-12">Let's learn some things about me, shall we?</h1>
@@ -82,8 +84,8 @@ export default function about() {
                             </div>
                         </div>
                         <h1 className="text-md md:text-xl 3xl:text-3xl opacity-90 px-16 mt-12">Looking for purpose in life.</h1>
-                        <h1 className="text-md md:text-xl 3xl:text-3xl opacity-90 px-16 text-[#f2f2f29b]">During my time post-graduation, I didn't exactly know what direction I wanted to take my life. The only prior experience I had was a bit of web development and mobile app development.</h1>
-                        <h1 className="text-md md:text-xl 3xl:text-3xl opacity-90 px-16 text-[#f2f2f29b] mb-12">While I was still dealing with my health issues, I began to learn web development from the basics. It took a couple months, but I eventually got through the Odin Project (a website that teaches users the basics of web development).</h1>
+                        <h1 className="text-md md:text-xl 3xl:text-3xl opacity-90 px-16 text-[#f2f2f29b]">During my time post-graduation, I didn't know what direction I wanted to go down in life. The only prior experience I had was a bit of web and mobile app development.</h1>
+                        <h1 className="text-md md:text-xl 3xl:text-3xl opacity-90 px-16 text-[#f2f2f29b] mb-12">While I was still dealing with my health issues, I began to learn web development back from the basics. The Odin Project is a website that teaches the fundamentals of front-end development and it took me a couple months to get through it.</h1>
                         <div className="rounded-[32px] md:p-4 border border-white/10 shadow-[0_40px_80px_rgba(0,0,0,0.5)] transition-all
                         duration-300 hover:-translate-y-1 hover:border-[#5a5a5a] hover:shadow-[0_0_60px_rgba(255,255,255,0.06)]">
                             <div className="relative flex overflow-hidden rounded-3xl bg-[linear-gradient(190deg,#252525,#121212)] border border-[#3d3d3d] shadow-[0_0_40px_rgba(255,255,255,0.03)] w-full">
@@ -98,8 +100,20 @@ export default function about() {
                         </div>
                         <h1 className="text-md md:text-xl 3xl:text-3xl opacity-90 px-16 mt-12">Outlook on life.</h1>
                         <h1 className="text-md md:text-xl 3xl:text-3xl opacity-90 px-16 text-[#f2f2f29b]">Everyday, I look back and think: "I was robbed of being able to live my life to the fullest." </h1>
-                        <h1 className="text-md md:text-xl 3xl:text-3xl opacity-90 px-16 text-[#f2f2f29b]">Now, I cherish every single day, mostly freed from the chains of illness. Picking up the pieces and moving forward with it all.</h1>
+                        <h1 className="text-md md:text-xl 3xl:text-3xl opacity-90 px-16 text-[#f2f2f29b]">Now, I cherish every single day, mostly freed from the chains of illness. Picking up the pieces and moving forward with it all . . .</h1>
                         <h1 className="text-md md:text-xl 3xl:text-3xl opacity-90 px-16 text-[#f2f2f29b] mb-12">Whenever I catch myself slowing down in life, I just remind myself that my past self wanted to be in the spot I am in right now . . . and my future self would be proud looking back at my efforts of today.</h1>
+                        <div className="rounded-[32px] md:p-4 border border-white/10 shadow-[0_40px_80px_rgba(0,0,0,0.5)] transition-all
+                        duration-300 hover:-translate-y-1 hover:border-[#5a5a5a] hover:shadow-[0_0_60px_rgba(255,255,255,0.06)]">
+                            <div className="relative flex overflow-hidden rounded-3xl bg-[linear-gradient(190deg,#252525,#121212)] border border-[#3d3d3d] shadow-[0_0_40px_rgba(255,255,255,0.03)] w-full">
+                                <Image
+                                    src="/lamp.jpg"
+                                    alt="Project Screenshot"
+                                    width={800}
+                                    height={1200}
+                                    className="w-full h-[600px] object-cover"
+                                />
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="z-2 opacity-20 mix-blend-overlay bg-[url('/noise.png')] [background-size:240px] absolute inset-0 pointer-events-none" />
